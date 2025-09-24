aa686026414d349d97a4a7fac4c6feff8dd1c93a
Boris Artarski <artarska6703@gmail.com>
2025-09-24 10:34:23 +0000

    ⚠ Cross origin request detected from 3000-firebase-studio-1757545853465.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
+    allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
   },
   typescript: {
     ignoreBuildErrors: true,
