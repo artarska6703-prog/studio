edb09f6976accf9c23d0c52b3a4f425f738df95d
Boris Artarski <artarska6703@gmail.com>
2025-09-24 14:19:55 +0000

    > nextn@0.1.0 dev > next dev --port 9002 --hostname 0.0.0.0
    Error: /hom

---
+++ next.config.js
@@ -0,0 +1,47 @@
+
+require('dotenv').config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
+  allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
+  experimental: {
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
