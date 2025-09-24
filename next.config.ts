3ee42daacc7d100048b52427f5b2936636c86214
Boris Artarski <artarska6703@gmail.com>
2025-09-24 10:35:57 +0000

    ✓ Starting...  ⚠ Invalid next.config.ts options detected:  ⚠     Unreco

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
+  allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
   experimental: {
-    // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
-    allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
   },
   typescript: {
     ignoreBuildErrors: true,
