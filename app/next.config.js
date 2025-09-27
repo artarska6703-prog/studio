967bbb95d4257b06faf0c549f3ae99f55c2f3ee7
Boris Artarski <artarska6703@gmail.com>
2025-09-27 18:14:24 +0000

    You're running into two **separate and critical issues**:
    ---
    
    ## 🧨 1.

---
+++ app/next.config.js
@@ -0,0 +1,49 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
+  allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'clipboard-write=*',
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https'
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig;
