ccee73a7884061694e7fe616193bdd866fe3370e
Boris Artarski <artarska6703@gmail.com>
2025-09-27 18:19:53 +0000

    You're very close to getting this working — that last message confirms t

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@
 const nextConfig = {
   /* config options here */
   // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
-  allowedDevOrigins: ["http://localhost:3000", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://*.cloudworkstations.dev",
+    "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"
+  ],
   async headers() {
     return [
       {
