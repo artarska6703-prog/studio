d5f2b1962705ad94f4db2aad91c6c36fb0ae383d
Boris Artarski <artarska6703@gmail.com>
2025-09-28 16:23:40 +0000

    ⨯ Failed to start server Error: listen EADDRINUSE: address already in us

--- next.config.js
+++ next.config.js
@@ -1,55 +1,59 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview.
-  allowedDevOrigins: ["http://localhost:3000", "https://*.cloudworkstations.dev", "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev"],
+  allowedDevOrigins: () => [
+    "http://localhost:9000",
+    "https://*.cloudworkstations.dev",
+    "https://*.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev",
+    "https://studio.firebase.google.com", // explicitly add this too
+  ],
+
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'Permissions-Policy',
-            value: 'clipboard-write=*',
+            key: "Permissions-Policy",
+            value: "clipboard-write=*",
           },
         ],
       },
     ];
   },
-  experimental: {
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https'
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
